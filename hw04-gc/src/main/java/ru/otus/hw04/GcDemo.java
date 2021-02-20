9aeb962cdf2f818b627befa26204d20e82db4612
VladimirChernikov <vldmrchernikov@gmail.com>
2021-02-20 18:55:55 +0000

    добавление постоянной нагрузки, пересмотр выводов

--- hw04-gc/src/main/java/ru/otus/hw04/GcDemo.java
+++ hw04-gc/src/main/java/ru/otus/hw04/GcDemo.java
@@ -1,19 +1,24 @@
 package ru.otus.hw04;
 
-import ru.otus.hw04.gc.JvmTests;
 import ru.otus.hw04.gc.GcLogger;
+import ru.otus.hw04.gc.JvmTests;
 
 public class GcDemo {
 
     public static void main(String... args) 
         throws Exception
     {
-        // log statistics every 60 seconds
-        GcLogger gcLogger = new GcLogger(60000);
+        // log GC statistics every n ms
+        GcLogger gcLogger = new GcLogger(10000);
         gcLogger.startMonitoring();
 
-        // exit with OutOfMemory after 300 seconds
-        JvmTests.runOutOfMemoryProcess(300000);
+        // start aside meamory consumption and log its activity every n ms
+        Thread constantLoadThread = JvmTests.startConstantMemoryLoad(10000);
+        
+        // exit with OutOfMemory after n ms
+        Thread linearlyIncreasingLoadThread = JvmTests.startIncreasingMemoryLoad(300000);
+        linearlyIncreasingLoadThread.join();
+        constantLoadThread.stop();
     }
 
 }
--- hw04-gc/src/main/java/ru/otus/hw04/gc/JvmTests.java
+++ hw04-gc/src/main/java/ru/otus/hw04/gc/JvmTests.java
@@ -2,6 +2,11 @@ package ru.otus.hw04.gc;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.message.ObjectArrayMessage;
 
 /**
  * Utility class to test JVM
@@ -11,40 +16,117 @@ public class JvmTests
 {
 
     /**
-     * Produce an OutOfMemory
+     * Start process with increasing memory load
      *  
-     * @param crashAfter seconds till expected OutOfMemory
+     * @param crashAfter milliseconds till expected OutOfMemory
      */
-    public static void runOutOfMemoryProcess(final long crashAfter)
+    public static Thread startIncreasingMemoryLoad(final long crashAfter)
             throws Exception
         {
-            // maxmimum heap memory -Xmx
-            final long maxMemory = Runtime.getRuntime().maxMemory();
-            // numberOfIterations till expected OOM
-            final long numberOfIterations = 1000;
-            final long step = ( maxMemory / numberOfIterations / 32 );
-            // pause between itertaions
-            final long delayValue = crashAfter / numberOfIterations;
-            // System.out.println(maxMemory+" "+ numberOfIterations+" "+ delayValue);
-            // dummy list of records
-            final List<String> report = new ArrayList<>();
-            int arraySize = 1;
-            // final long beginTime = System.currentTimeMillis();
-            for ( long iter = 1; iter <= numberOfIterations * 2; iter++ ) {
-                for ( int i = 0; i < arraySize; i++ ) {
-                    final String record = new String("record0"); // occupies about 32 bytes
-                    if ( report.size() <= i ) {
-                        report.add( i, record );
+            Runnable loadTask = () -> {
+                // maxmimum heap memory -Xmx
+                final long maxMemory = Runtime.getRuntime().maxMemory();
+                // numberOfIterations till expected OOM
+                final long numberOfIterations = 1000;
+                final long step = ( maxMemory / numberOfIterations / 16 );
+                // pause between itertaions
+                final long delayValue = crashAfter / numberOfIterations;
+                // dummy list of records
+                final List<String> report = new ArrayList<>();
+                int arraySize = 1;
+                while ( !Thread.interrupted() ) {
+                    for ( int i = 0; i < arraySize; i++ ) {
+                        final String record = new String("record0"); // occupies about 32 bytes
+                        if ( report.size() <= i ) {
+                            report.add( i, record );
+                        }
+                        else {
+                            report.set( i, record );
+                        }
                     }
-                    else {
-                        report.set( i, record );
+                    arraySize += step;
+                    try {
+                        Thread.sleep( delayValue );
+                    } catch(Exception e){
+                        e.printStackTrace();
                     }
-                }
-                arraySize += step;
-                Thread.sleep( delayValue );
-                // System.out.println("time:" + (System.currentTimeMillis() - beginTime) / 1000);
+                    }
+                return;
+                };
+                Thread thread = new Thread(loadTask);
+                thread.start();
+                return thread;
             }
-        }
+
+    /**
+     * Start process with constant memory load
+     *  
+     * @param logEvery period of reporting, in ms
+     */
+    public static Thread startConstantMemoryLoad( int logEvery ) {
+        Runnable loadTask = () -> {
+
+            final long sleepBetweenIterations = 10;
+
+            final Logger LOG = LogManager.getLogger("jvmtests.csv");
+            long beginTime = System.currentTimeMillis();
+            long logCount = 0l;
+
+            final Random random = new Random(0);
+            List<String> report;
+
+            final int listSizeMin = 100;
+            final double listSizeDev = 100d;
+            int listSize = 0;
+
+            final int stringSizeMin = 10;
+            final double stringSizeDev = 1000d;
+            int stringSize = 0;
+
+            double consumedValue = 0d;
+            while ( !Thread.interrupted() ) {
+
+                // Generate list of random-length strings
+                report = new ArrayList<>();
+                listSize = (int)( Math.log(1-random.nextDouble())*(-listSizeDev) )+listSizeMin;
+                // listSize = Math.abs( (int) ( (Math.pow(random.nextGaussian(),1)*listSizeDev) + listSizeMin ) )+1;
+                for ( int i = 0 ; i < listSize; i++ ) {
+                    stringSize = (int)( Math.log(1-random.nextDouble())*(-stringSizeDev) )+stringSizeMin;
+                    // stringSize = Math.abs( (int) ( (Math.pow(random.nextGaussian(),1)*stringSizeDev) + stringSizeMin ) )+1;
+                    String str = "test";
+                    str = str.repeat(stringSize);
+                    report.add(str);
+                    consumedValue += stringSize;
+                }
+
+                // Make some calculations
+                Double dummyValue = 1d;
+                for ( String str : report ) {
+                    dummyValue *= ( (str.hashCode() % 10000) * 0.1 ) ;
+                }
+
+                // Sleep
+                try {
+                    Thread.sleep(sleepBetweenIterations);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                // Log statistics
+                long startTime = System.currentTimeMillis() - beginTime;
+                if ( startTime - logEvery * logCount > logEvery ) {
+                    logCount = ( startTime/logEvery );
+                    LOG.info( new ObjectArrayMessage( "constant load", startTime, consumedValue ) );
+                    consumedValue = 0d;
+                }
+            }
+            return;
+        };
+        Thread thread = new Thread(loadTask);
+        thread.start();
+        return thread;
+    }
+
 }
 
 
