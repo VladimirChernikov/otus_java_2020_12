3ad537ce2edc8fab50855078ac9399775dd70f52
VladimirChernikov <vldmrchernikov@gmail.com>
2021-04-04 19:43:30 +0000

    hw10-hibernate

---
+++ hw10-hibernate/src/main/java/ru/otus/crm/model/Client.java
@@ -0,0 +1,107 @@
+package ru.otus.crm.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "client")
+public class Client implements Cloneable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_client")
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToOne(mappedBy = "client", cascade = { CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH }, fetch = FetchType.EAGER, orphanRemoval = true)
+    private Address address;
+
+    @OneToMany(mappedBy = "client", cascade = { CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH }, fetch = FetchType.EAGER, orphanRemoval = true)
+    private List<Phone> phone = new ArrayList<>();
+
+    public Client() {
+    }
+
+    public Client(String name) {
+        this.id = null;
+        this.name = name;
+    }
+
+    public Client(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Client(Long id, String name, Address address, List<Phone> phone) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    @Override
+    public Client clone() {
+        return new Client( 
+                this.id,
+                this.name, 
+                this.address != null ? this.address.clone() : null, 
+                this.phone.stream().map( Phone::clone ).collect(Collectors.toList()) 
+                );
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Client [address=" + address + ", id=" + id + ", name=" + name + ", phone=" + phone + "]";
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        address.setClient(this);
+        this.address = address;
+    }
+
+    public List<Phone> getPhone() {
+        return phone;
+    }
+
+    public void setPhone(List<Phone> phones) {
+        for (var phone : phones ) {
+            phone.setClient(this);
+        }
+        this.phone = phones;
+    }
+
+}
